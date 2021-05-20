2d80b0588b1bec7e477514932788f758b00d1fe2
Santhosh Ram.S <santhoshram7117@gmail.com>
2021-05-20 15:11:42 +0530

    algin.py

--- algin.py
+++ algin.py
@@ -15,8 +15,10 @@ citation_author=soup.find('meta',attrs = {'name':'citation_author'})['content']
 citation_volume=soup.find('meta',attrs = {'name':'citation_volume'})['content']
 citation_publication_number=soup.find('meta',attrs = {'name':'citation_publication_number'})['content']
 citation_keywords=soup.find('meta',attrs = {'name':'citation_keywords'})['content']
-prism_publicationDate=soup.find('meta',attrs = {'class':'prism.publicationDate'})['content']
-#Artical_type=soup.find('meta',attrs = {'name':'js-article-abstract-container'})['id']
+prism_publicationDate=soup.find('meta',attrs = {'name':'prism.publicationDate'})['content']
+description=soup.find('meta',attrs = {'name':'dc.description'})['content']
+citation_description=soup.find('meta',attrs = {'name':'citation_description'})['content']
+citation_abstract=soup.find('meta',attrs = {'name':'citation_abstract'})['content']
 #print(pdf_url)
 #print(Title)
 
@@ -28,12 +30,17 @@ scraping_dict['pdf_url']=pdf_url
 scraping_dict['citation_author']=citation_author
 scraping_dict['citation_volume']=citation_volume
 scraping_dict['prism_publicationDate']=prism_publicationDate
-#scraping_dict['Artical_type']=Artical_type
+scraping_dict['description']=description
+scraping_dict['citation_abstract']=citation_abstract
+scraping_dict['citation_description']=citation_description
 print(scraping_dict)
 print(pdf_url)
 print(citation_author)
 print(citation_volume)
-print(citation_publication_number)
+
 print(citation_keywords)
 print(prism_publicationDate)
-#print(Artical_type)
+print(description)
+print(citation_description)
+print(citation_publication_number)
+print(citation_abstract)
